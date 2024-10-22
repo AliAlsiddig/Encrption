42fb1b8c5f700da644d2d6c88791b6d95bcc1b49
Ali Alsidig <aloshsoftware@gmail.com>
2024-10-22 15:37:16 -0700

    [modifocation] add new method of encription

--- encrption.java
+++ encrption.java
@@ -15,7 +15,8 @@ public class Encrption {
         // TODO code application logic here
         Scanner scanner = new Scanner(System.in);
         System.out.println("Select encription Algrithm : "
-                + "\n1 -  Caesar cipher. \n");
+                + "\n1 -  Caesar cipher. \n"
+                + "2 - Substitution Cipher");
         int selection = scanner.nextInt();
         System.out.print("plain text : ");
         scanner.nextLine();
@@ -28,7 +29,13 @@ public class Encrption {
                 String res = CaesarCipher(plain);
                 System.out.println("cipher text :" + res);
                 break;
-           
+            case 2:
+                System.out.print("key : ");
+                int key = scanner.nextInt();
+                String res2 =SubstitutionCipher(plain, key);
+                System.out.println("cipher text :" + res2);
+
+                break;
             default:
                 break;
         }
@@ -56,6 +63,25 @@ public class Encrption {
 
     }
 
-   
+    private static String SubstitutionCipher(String plain, int key) {
+        String cipher = "";
+        for (int i = 0; i < plain.length(); i++) {
+            if (plain.charAt(i) != ' ') {
+                if (Character.isUpperCase(plain.charAt(i))) {
+                    int ciphchar = ((((int) plain.charAt(i)) - 65) + key) % 26;
+                    ciphchar += 65;
+                    cipher += (char) ciphchar;
+                } else {
+                     int ciphchar = ((((int) plain.charAt(i)) - 97) + key) % 26;
+                    ciphchar += 97;
+                    cipher += (char) ciphchar;
+                }
+
+            } else {
+                cipher += ' ';
+            }
+        }
+        return cipher;
+    }
 
 }
